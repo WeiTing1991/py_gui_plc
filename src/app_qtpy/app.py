80004eb4cd6bfb3b02929e8d7341d37fe9eb055d
WeitingChen <weitingchen0502@gmail.com>
2022-11-17 02:25:14 +0100

    new setup app

--- src/app_qtpy/app.py
+++ src/app_qtpy/app.py
@@ -2,12 +2,11 @@ import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
 
 
-
 class ControllerApp():
     def __init__(self,
                 title: str='ControllerApp_v1',
-                width: int = 1920,
-                height: int = 1080,
+                width: int = 1200,
+                height: int = 800,
                 ):
         super().__init__()
         app = QApplication(sys.argv)
@@ -32,6 +31,5 @@ class ControllerApp():
 
 
 if __name__ == "__main__":
-    
     app = ControllerApp()
     app.run()
